ad5dd2bf5ca70e0e647bd5aca63e2f6cecfb3fcb
Johannes Sarpola <johannes.sarpola@gmail.com>
2023-01-17 19:01:20 +0200

    add starters for web and desktop

---
+++ chat-web-ui/src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+import Chat from './components/Chat'
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Chat></Chat>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
---
+++ chat-web-ui/src/components/Chat.tsx
@@ -0,0 +1,15 @@
+import { Text } from '@chakra-ui/react'
+import './Chat.css';
+
+export default function Chat() {
+    return (
+        <>
+            <Text fontSize='6xl'> Hello world </Text>
+            <Text fontSize='md'> I'm a paragraph </Text>
+            <Text fontSize='md'> I'm a paragraph </Text>
+            <Text fontSize='md'> I'm a paragraph </Text>
+            <Text fontSize='md'> I'm a paragraph </Text>
+            <Text fontSize='md'> I'm a paragraph </Text>
+        </>
+    );
+}

9a42205369b8c17b4dbea18a44d7c0063bbd425f
Johannes Sarpola <johannes.sarpola@gmail.com>
2023-01-24 18:24:48 +0200

    continued with grpc

---
+++ backends/chat-backend/app/server_grpc.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	//"context"
+	//"log"
+	//"time"
+
+	api "github.com/johannessarpola/go-websockets-chat/api/v1"
+	"google.golang.org/grpc"
+	//"google.golang.org/grpc"
+	//codes "google.golang.org/grpc/codes"
+	//status "google.golang.org/grpc/status"
+)
+
+type grpcServer struct {
+	api.UnimplementedChatServer
+	// TODO Pulsar producer
+	// TODO Pulsar consumer
+}
+
+func NewGRPCServer() *grpc.Server {
+
+	var err error
+	gsrv := grpc.NewServer()
+	srv := grpcServer{
+		// TODO Pulsar producer
+		// TODO Pulsar consumer
+	}
+	api.RegisterChatServer(gsrv, &srv)
+	return gsrv
+}
